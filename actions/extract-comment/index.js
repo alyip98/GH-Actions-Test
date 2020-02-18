18086a3a2305077272748652bcdf35b0b9d4d1f1
alyip98 <alyip98@gmail.com>
2020-02-18 21:46:31 +0800

    Parse comment for path and version

--- actions/extract-comment/index.js
+++ actions/extract-comment/index.js
@@ -1,8 +1,16 @@
 const core = require('@actions/core');
 
 try {
-  const event = core.getInput('event', {required: true});
-  core.setOutput('path', 'thisisthepathoutoput');
+  const event = JSON.parse(core.getInput('event', {required: true}));
+  const commentBody = event['comment']['body'];
+  const pattern = /!build \[\S*]\((\S*)\) (\S*)/;
+  const matches = commentBody.match(pattern);
+
+  const path = matches[1];
+  const version = matches[2] || '';
+
+  core.setOutput('path', path);
+  core.setOutput('version', version);
 } catch (error) {
   core.setFailed(error.message);
 }
